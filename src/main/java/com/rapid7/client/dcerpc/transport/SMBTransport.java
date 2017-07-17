3054b9f10d22d54ecbd74d80f0732f1b73ef7d98
pmiseiko-r7 <pmiseiko-r7@users.noreply.github.com>
2017-07-17 18:57:19 -0400

    Update SMBj version and remove impersonation code that is now available in SMBj.  Increment library version.

--- src/main/java/com/rapid7/client/dcerpc/transport/SMBTransport.java
+++ src/main/java/com/rapid7/client/dcerpc/transport/SMBTransport.java
@@ -19,36 +19,16 @@
 package com.rapid7.client.dcerpc.transport;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import com.hierynomus.mssmb2.SMB2Dialect;
-import com.hierynomus.mssmb2.SMB2FileId;
-import com.hierynomus.mssmb2.messages.SMB2IoctlRequest;
-import com.hierynomus.mssmb2.messages.SMB2IoctlRequest.ControlCode;
-import com.hierynomus.mssmb2.messages.SMB2IoctlResponse;
-import com.hierynomus.smbj.session.Session;
+import com.hierynomus.smbj.share.NamedPipe;
 import com.rapid7.client.dcerpc.RPCRequest;
 import com.rapid7.client.dcerpc.RPCResponse;
 
 public class SMBTransport implements RPCTransport {
-    private final Session session;
-    private final SMB2Dialect negotiatedDialect;
-    private final SMB2FileId fileId;
-    private final long sessionId;
-    private final long treeId;
+    private final NamedPipe namedPipe;
     private int callID = 1;
 
-    public SMBTransport(
-        final Session session,
-        final SMB2Dialect negotiatedDialect,
-        final SMB2FileId fileId,
-        final long sessionId,
-        final long treeId) {
-        this.session = session;
-        this.negotiatedDialect = negotiatedDialect;
-        this.fileId = fileId;
-        this.sessionId = sessionId;
-        this.treeId = treeId;
+    public SMBTransport(final NamedPipe namedPipe) {
+        this.namedPipe = namedPipe;
     }
 
     @Override
@@ -56,18 +36,7 @@ public class SMBTransport implements RPCTransport {
         throws IOException {
         final int callID = this.callID++;
         final byte[] requestBytes = request.marshal(callID);
-        final SMB2IoctlRequest ioctlRequest = new SMB2IoctlRequest(negotiatedDialect, sessionId, treeId,
-            ControlCode.FSCTL_PIPE_TRANSCEIVE, fileId, requestBytes, true);
-        final Future<SMB2IoctlResponse> future = session.send(ioctlRequest);
-        final SMB2IoctlResponse response;
-        try {
-            response = future.get();
-        } catch (final ExecutionException | InterruptedException exception) {
-            final IOException ioException = new IOException();
-            ioException.addSuppressed(exception);
-            throw ioException;
-        }
-        final byte[] responseBytes = response.getOutputBuffer();
+        final byte[] responseBytes = namedPipe.transact(requestBytes);
         return request.unmarshal(responseBytes, callID);
     }
 }
--- src/main/java/com/rapid7/client/dcerpc/transport/SMBTransportFactories.java
+++ src/main/java/com/rapid7/client/dcerpc/transport/SMBTransportFactories.java
@@ -19,36 +19,20 @@
 package com.rapid7.client.dcerpc.transport;
 
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.util.EnumSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.concurrent.Future;
 import com.hierynomus.msdtyp.AccessMask;
-import com.hierynomus.mserref.NtStatus;
-import com.hierynomus.msfscc.FileAttributes;
 import com.hierynomus.mssmb2.SMB2CreateDisposition;
-import com.hierynomus.mssmb2.SMB2CreateOptions;
-import com.hierynomus.mssmb2.SMB2Dialect;
-import com.hierynomus.mssmb2.SMB2FileId;
+import com.hierynomus.mssmb2.SMB2ImpersonationLevel;
 import com.hierynomus.mssmb2.SMB2ShareAccess;
-import com.hierynomus.mssmb2.messages.SMB2CreateResponse;
-import com.hierynomus.protocol.commons.concurrent.Futures;
-import com.hierynomus.smbj.common.SMBApiException;
-import com.hierynomus.smbj.common.SMBException;
-import com.hierynomus.smbj.common.SMBRuntimeException;
-import com.hierynomus.smbj.connection.Connection;
-import com.hierynomus.smbj.connection.NegotiatedProtocol;
 import com.hierynomus.smbj.session.Session;
+import com.hierynomus.smbj.share.NamedPipe;
+import com.hierynomus.smbj.share.PipeShare;
 import com.hierynomus.smbj.share.Share;
-import com.hierynomus.smbj.share.TreeConnect;
 import com.hierynomus.smbj.transport.TransportException;
 import com.rapid7.client.dcerpc.Interface;
 import com.rapid7.client.dcerpc.RPCResponse;
 import com.rapid7.client.dcerpc.messages.Bind;
 import com.rapid7.client.dcerpc.messages.BindACK;
-import com.rapid7.client.smb2.SMB2ImpersonationLevel;
-import com.rapid7.client.smb2.messages.SMB2CreateRequest;
 
 public enum SMBTransportFactories {
     WINREG("winreg", Interface.WINREG_V1_0, Interface.NDR_32BIT_V2),
@@ -56,114 +40,37 @@ public enum SMBTransportFactories {
 
     private final static int STATUS_PIPE_NOT_AVAILABLE_BACKOFF_TIME_MS = 3000;
     private final static int STATUS_PIPE_NOT_AVAILABLE_RETRIES = 1;
-    private final String path;
+    private final String name;
     private final Interface abstractSyntax;
     private final Interface transferSyntax;
 
     private SMBTransportFactories(final String path, final Interface abstractSyntax, final Interface transferSyntax) {
-        this.path = path;
+        this.name = path;
         this.abstractSyntax = abstractSyntax;
         this.transferSyntax = transferSyntax;
     }
 
     public RPCTransport getTransport(final Session session)
         throws IOException {
-        final Connection connection = session.getConnection();
-        final NegotiatedProtocol negotiatedProtocol = connection.getNegotiatedProtocol();
-        final SMB2Dialect dialect = negotiatedProtocol.getDialect();
-        final long sessionID = session.getSessionId();
         final Share share = session.connectShare("IPC$");
-        final TreeConnect tree = share.getTreeConnect();
-        final long treeID = tree.getTreeId();
-        final SMB2FileId fileID = openNamedPipe(share, path, STATUS_PIPE_NOT_AVAILABLE_RETRIES);
-        final SMBTransport transport = new SMBTransport(session, dialect, fileID, sessionID, treeID);
+        if (share instanceof PipeShare) {
+            final PipeShare pipeShare = (PipeShare) share;
+            final NamedPipe namedPipe = pipeShare.open(name, SMB2ImpersonationLevel.Impersonation,
+                EnumSet.of(AccessMask.MAXIMUM_ALLOWED), null,
+                EnumSet.of(SMB2ShareAccess.FILE_SHARE_READ, SMB2ShareAccess.FILE_SHARE_WRITE),
+                SMB2CreateDisposition.FILE_OPEN_IF, null);
+            final SMBTransport transport = new SMBTransport(namedPipe);
+            final Bind bind = new Bind(abstractSyntax, transferSyntax);
+            final RPCResponse response = transport.transact(bind);
 
-        final Bind bind = new Bind(abstractSyntax, transferSyntax);
-        final RPCResponse response = transport.transact(bind);
-
-        if (response instanceof BindACK) {
-            return new SMBTransport(session, dialect, fileID, sessionID, treeID);
-        }
-
-        throw new TransportException(
-            String.format("BIND %s (%s -> %s) failed.", abstractSyntax.getName(), path, abstractSyntax.getRepr()));
-    }
-
-    private static SMB2FileId openNamedPipe(final Share share, final String path, final int retryLimit)
-        throws IOException {
-        final Queue<SMBApiException> exceptions = new LinkedList<>();
-        for (int retry = -1; retry < retryLimit; retry++) {
-            try {
-                return openNamedPipe(share, path);
-            } catch (final SMBApiException exception) {
-                exceptions.add(exception);
-                switch (exception.getStatus()) {
-                case STATUS_PIPE_NOT_AVAILABLE:
-                    // XXX: There has to be a better way to do this...
-                    try {
-                        Thread.sleep(STATUS_PIPE_NOT_AVAILABLE_BACKOFF_TIME_MS);
-                    } catch (final InterruptedException iException) {
-                        final InterruptedIOException iioException = new InterruptedIOException();
-                        iioException.addSuppressed(iException);
-                        throw iioException;
-                    }
-                    break;
-                default:
-                    throw new SMBException(exceptions.poll());
-                }
-            }
-        }
-        throw new SMBException(exceptions.poll());
-    }
-
-    private static SMB2FileId openNamedPipe(final Share share, final String namedPipe) {
-        return open(share.getTreeConnect(), namedPipe, SMB2ImpersonationLevel.IMPERSONATION,
-            AccessMask.MAXIMUM_ALLOWED.getValue(), null,
-            EnumSet.of(SMB2ShareAccess.FILE_SHARE_READ, SMB2ShareAccess.FILE_SHARE_WRITE),
-            SMB2CreateDisposition.FILE_OPEN_IF, null);
-    }
-
-    private static SMB2FileId open(
-        final TreeConnect treeConnect,
-        final String path,
-        final SMB2ImpersonationLevel impersonationLevel,
-        final long accessMask,
-        final EnumSet<FileAttributes> fileAttributes,
-        final EnumSet<SMB2ShareAccess> shareAccess,
-        final SMB2CreateDisposition createDisposition,
-        final EnumSet<SMB2CreateOptions> createOptions)
-        throws SMBApiException {
-        final Session session = treeConnect.getSession();
-        final SMB2CreateRequest cr = openFileRequest(treeConnect, path, impersonationLevel, accessMask, shareAccess,
-            fileAttributes, createDisposition, createOptions);
-        try {
-            final Future<SMB2CreateResponse> responseFuture = session.send(cr);
-            final SMB2CreateResponse cresponse = Futures.get(responseFuture, SMBRuntimeException.Wrapper);
-            if (cresponse.getHeader().getStatus() != NtStatus.STATUS_SUCCESS) {
-                throw new SMBApiException(cresponse.getHeader(), "Create failed for " + path);
+            if (response instanceof BindACK) {
+                return transport;
             }
 
-            return cresponse.getFileId();
-        } catch (final TransportException exception) {
-            throw SMBRuntimeException.Wrapper.wrap(exception);
+            throw new TransportException(
+                String.format("BIND %s (%s -> %s) failed.", abstractSyntax.getName(), name, abstractSyntax.getRepr()));
         }
 
-    }
-
-    private static SMB2CreateRequest openFileRequest(
-        final TreeConnect treeConnect,
-        final String path,
-        final SMB2ImpersonationLevel impersonationLevel,
-        final long accessMask,
-        final EnumSet<SMB2ShareAccess> shareAccess,
-        final EnumSet<FileAttributes> fileAttributes,
-        final SMB2CreateDisposition createDisposition,
-        final EnumSet<SMB2CreateOptions> createOptions) {
-        final Session session = treeConnect.getSession();
-        final SMB2CreateRequest createRequest = new SMB2CreateRequest(
-            session.getConnection().getNegotiatedProtocol().getDialect(), session.getSessionId(),
-            treeConnect.getTreeId(), impersonationLevel, accessMask, fileAttributes, shareAccess, createDisposition,
-            createOptions, path);
-        return createRequest;
+        throw new TransportException(String.format("%s not a named pipe.", name));
     }
 }
