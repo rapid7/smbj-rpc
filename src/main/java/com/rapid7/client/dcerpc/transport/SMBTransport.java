9c19487f6b62ea30eb75dda4f33757881e76216f
pmiseiko-r7 <pmiseiko-r7@users.noreply.github.com>
2017-08-03 15:04:05 -0400

    Reduce the transaction buffer size from the default 1MB value to 64KB.  This fixed an issue with Windows 2012 R2 returning INVALID_PARAMETER when using the maximum transaction buffer size parsed out of the negotiate protocol response.  Handle a BUFFER_OVERFLOW when 64KB is not large enough for the complete transaction response.  The named pipe buffer on the server side must first be drained.  SMBj does not return the partial data returned in the transaction response.  Data must exist that follows what the transaction response contained otherwise the transaction response status would not be BUFFER_OVERFLOW.  The RPC request is written to the named pipe using a write request instead of using a transaction request and one or more read requests read the RPC response.  The Windows 2012 R2 system that returned INVALID_PARAMETER when using a transaction buffer of 1MB does not have an issue when using a read buffer of 1MB.

--- src/main/java/com/rapid7/client/dcerpc/transport/SMBTransport.java
+++ src/main/java/com/rapid7/client/dcerpc/transport/SMBTransport.java
@@ -18,12 +18,18 @@
  */
 package com.rapid7.client.dcerpc.transport;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import com.hierynomus.mserref.NtStatus;
+import com.hierynomus.mssmb2.messages.SMB2ReadResponse;
+import com.hierynomus.smbj.common.SMBApiException;
 import com.hierynomus.smbj.share.NamedPipe;
 import com.rapid7.client.dcerpc.RPCRequest;
 import com.rapid7.client.dcerpc.RPCResponse;
 
 public class SMBTransport implements RPCTransport {
+    private final static int MAXIMUM_TRANSACT_SIZE = 65536;
+    private final static int MAXIMUM_READ_SIZE = 65536;
     private final NamedPipe namedPipe;
     private int callID = 1;
 
@@ -36,7 +42,30 @@ public class SMBTransport implements RPCTransport {
         throws IOException {
         final int callID = this.callID++;
         final byte[] requestBytes = request.marshal(callID);
-        final byte[] responseBytes = namedPipe.transact(requestBytes);
-        return request.unmarshal(responseBytes, callID);
+        try {
+            final byte[] responseBytes = new byte[MAXIMUM_TRANSACT_SIZE];
+            namedPipe.transact(requestBytes, responseBytes);
+            return request.unmarshal(responseBytes, callID);
+        } catch (final SMBApiException exception) {
+            final NtStatus status = exception.getStatus();
+            if (status.equals(NtStatus.STATUS_BUFFER_OVERFLOW)) {
+                for (;;) {
+                    final SMB2ReadResponse readResponse = namedPipe.read(MAXIMUM_READ_SIZE);
+                    if (0 >= readResponse.getDataRemaining()) {
+                        break;
+                    }
+                }
+            }
+            final ByteArrayOutputStream responseBytes = new ByteArrayOutputStream();
+            namedPipe.write(requestBytes);
+            for (;;) {
+                final SMB2ReadResponse readResponse = namedPipe.read(MAXIMUM_READ_SIZE);
+                responseBytes.write(readResponse.getData());
+                if (0 >= readResponse.getDataRemaining()) {
+                    break;
+                }
+            }
+            return request.unmarshal(responseBytes.toByteArray(), callID);
+        }
     }
 }
