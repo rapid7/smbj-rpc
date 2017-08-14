63b79f74b241819c6df6d476ba996f28760c0011
pmiseiko-r7 <pmiseiko-r7@users.noreply.github.com>
2017-08-14 18:55:30 -0400

    The logic for change 9c19487f6b62ea30eb75dda4f33757881e76216f is getting pushed into the SMBj repository (see the PR at https://github.com/hierynomus/smbj/pull/155).

--- src/main/java/com/rapid7/client/dcerpc/transport/SMBTransport.java
+++ src/main/java/com/rapid7/client/dcerpc/transport/SMBTransport.java
@@ -18,18 +18,13 @@
  */
 package com.rapid7.client.dcerpc.transport;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import com.hierynomus.mserref.NtStatus;
-import com.hierynomus.mssmb2.messages.SMB2ReadResponse;
-import com.hierynomus.smbj.common.SMBApiException;
 import com.hierynomus.smbj.share.NamedPipe;
 import com.rapid7.client.dcerpc.RPCRequest;
 import com.rapid7.client.dcerpc.RPCResponse;
 
 public class SMBTransport implements RPCTransport {
-    private final static int MAXIMUM_TRANSACT_SIZE = 65536;
-    private final static int MAXIMUM_READ_SIZE = 65536;
+    private final static int MAXIMUM_TRANSACT_SIZE = 262144;
     private final NamedPipe namedPipe;
     private int callID = 1;
 
@@ -40,32 +35,13 @@ public class SMBTransport implements RPCTransport {
     @Override
     public <T extends RPCResponse> T transact(final RPCRequest<T> request)
         throws IOException {
-        final int callID = this.callID++;
-        final byte[] requestBytes = request.marshal(callID);
-        try {
-            final byte[] responseBytes = new byte[MAXIMUM_TRANSACT_SIZE];
-            namedPipe.transact(requestBytes, responseBytes);
-            return request.unmarshal(responseBytes, callID);
-        } catch (final SMBApiException exception) {
-            final NtStatus status = exception.getStatus();
-            if (status.equals(NtStatus.STATUS_BUFFER_OVERFLOW)) {
-                for (;;) {
-                    final SMB2ReadResponse readResponse = namedPipe.read(MAXIMUM_READ_SIZE);
-                    if (0 >= readResponse.getDataRemaining()) {
-                        break;
-                    }
-                }
-            }
-            final ByteArrayOutputStream responseBytes = new ByteArrayOutputStream();
-            namedPipe.write(requestBytes);
-            for (;;) {
-                final SMB2ReadResponse readResponse = namedPipe.read(MAXIMUM_READ_SIZE);
-                responseBytes.write(readResponse.getData());
-                if (0 >= readResponse.getDataRemaining()) {
-                    break;
-                }
-            }
-            return request.unmarshal(responseBytes.toByteArray(), callID);
+        final int callID;
+        synchronized (this) {
+            callID = this.callID++;
         }
+        final byte[] requestBytes = request.marshal(callID);
+        final byte[] responseBytes = new byte[MAXIMUM_TRANSACT_SIZE];
+        namedPipe.transact(requestBytes, responseBytes);
+        return request.unmarshal(responseBytes, callID);
     }
 }
