d8934fcc52ecb6abfbb1be100d820b5eaa8c2a08
Sangdo lee <sangdo_lee@rapid7.com>
2017-11-02 10:28:23 -0400

    update copyright header and apply code style

--- src/main/java/com/rapid7/client/dcerpc/mssamr/messages/SamrOpenUserRequest.java
+++ src/main/java/com/rapid7/client/dcerpc/mssamr/messages/SamrOpenUserRequest.java
@@ -1,8 +1,21 @@
-/***************************************************************************
- * COPYRIGHT (C) 2017, Rapid7 LLC, Boston, MA, USA.
- * All rights reserved. This material contains unpublished, copyrighted
- * work including confidential and proprietary information of Rapid7.
- **************************************************************************/
+/**
+ * Copyright 2017, Rapid7, Inc.
+ *
+ * License: BSD-3-clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ */
 package com.rapid7.client.dcerpc.mssamr.messages;
 
 import java.io.IOException;
@@ -10,9 +23,6 @@ import com.rapid7.client.dcerpc.io.PacketOutput;
 import com.rapid7.client.dcerpc.messages.RequestCall;
 import com.rapid7.client.dcerpc.mssamr.objects.DomainHandle;
 
-/**
- *
- */
 public class SamrOpenUserRequest extends RequestCall<SamrOpenUserResponse> {
     public final static short OP_NUM = 34;
 
@@ -26,8 +36,8 @@ public class SamrOpenUserRequest extends RequestCall<SamrOpenUserResponse> {
     }
 
     @Override
-    public void marshal(PacketOutput packetOut)
-        throws IOException {
+    public void marshal(PacketOutput packetOut) throws IOException {
+
 	packetOut.write(handle.getBytes());
 	// Generic rights: 0x00000000
 	// Standard rights: 0x00020000
@@ -36,8 +46,8 @@ public class SamrOpenUserRequest extends RequestCall<SamrOpenUserResponse> {
 	// - Groups: SAMR_USER_ACCESS_GET_GROUPS is SET
 	// - Attributes: SAMR_USER_ACCESS_GET_ATTRIBUTES is SET
 	// - Logoninfo: SAMR_USER_ACCESS_GET_LOGONINFO is SET
-        packetOut.writeInt(0x2011B);
-        packetOut.writeInt(userRid);
+	packetOut.writeInt(0x2011B);
+	packetOut.writeInt(userRid);
     }
 
     @Override
