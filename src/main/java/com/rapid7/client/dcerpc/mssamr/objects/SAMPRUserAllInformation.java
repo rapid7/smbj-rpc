7d36d09c14e0d2a997397f8f61fd23009e47cf3d
Taylor Osmun <Taylor_Osmun@rapid7.com>
2017-11-13 10:39:22 -0500

    Adding getters/setters

--- src/main/java/com/rapid7/client/dcerpc/mssamr/objects/SAMPRUserAllInformation.java
+++ src/main/java/com/rapid7/client/dcerpc/mssamr/objects/SAMPRUserAllInformation.java
@@ -30,7 +30,39 @@ import com.rapid7.client.dcerpc.objects.RPCUnicodeString;
 
 /**
  * <b>Alignment: 8</b><pre>
- *     TODO</pre>
+ *     OLD_LARGE_INTEGER LastLogon;: 8
+ *     OLD_LARGE_INTEGER LastLogoff;: 8
+ *     OLD_LARGE_INTEGER PasswordLastSet;: 8
+ *     OLD_LARGE_INTEGER AccountExpires;: 8
+ *     OLD_LARGE_INTEGER PasswordCanChange;: 8
+ *     OLD_LARGE_INTEGER PasswordMustChange;: 8
+ *     RPC_UNICODE_STRING UserName;: 4
+ *     RPC_UNICODE_STRING FullName;: 4
+ *     RPC_UNICODE_STRING HomeDirectory;: 4
+ *     RPC_UNICODE_STRING HomeDirectoryDrive;: 4
+ *     RPC_UNICODE_STRING ScriptPath;: 4
+ *     RPC_UNICODE_STRING ProfilePath;: 4
+ *     RPC_UNICODE_STRING AdminComment;: 4
+ *     RPC_UNICODE_STRING WorkStations;: 4
+ *     RPC_UNICODE_STRING UserComment;: 4
+ *     RPC_UNICODE_STRING Parameters;: 4
+ *     RPC_SHORT_BLOB LmOwfPassword;: 4
+ *     RPC_SHORT_BLOB NtOwfPassword;: 4
+ *     RPC_UNICODE_STRING PrivateData;: 4
+ *     SAMPR_SR_SECURITY_DESCRIPTOR SecurityDescriptor;: 4
+ *     unsigned long UserId;: 4
+ *     unsigned long PrimaryGroupId;: 4
+ *     unsigned long UserAccountControl; 4
+ *     unsigned long WhichFields; 4
+ *     SAMPR_LOGON_HOURS LogonHours;: 4
+ *     unsigned short BadPasswordCount;: 2
+ *     unsigned short LogonCount;: 2
+ *     unsigned short CountryCode;: 2
+ *     unsigned short CodePage;: 2
+ *     unsigned char LmPasswordPresent;: 1
+ *     unsigned char NtPasswordPresent;: 1
+ *     unsigned char PasswordExpired;: 1
+ *     unsigned char PrivateDataSensitive;: 1</pre>
  * <a href="https://msdn.microsoft.com/en-us/library/cc245622.aspx">SAMPR_USER_ALL_INFORMATION</a>
  * <blockquote><pre>The SAMPR_USER_ALL_INFORMATION structure contains user attribute information. Most fields are described in section 2.2.7.1. The exceptions are described below.
  *      typedef struct _SAMPR_USER_ALL_INFORMATION {
@@ -113,6 +145,270 @@ public class SAMPRUserAllInformation implements Unmarshallable {
     private boolean ntPasswordPresent;
     private boolean passwordExpired;
     private boolean privateDataSensitive;
+
+    public long getLastLogon() {
+        return lastLogon;
+    }
+
+    public void setLastLogon(long lastLogon) {
+        this.lastLogon = lastLogon;
+    }
+
+    public long getLastLogoff() {
+        return lastLogoff;
+    }
+
+    public void setLastLogoff(long lastLogoff) {
+        this.lastLogoff = lastLogoff;
+    }
+
+    public long getPasswordLastSet() {
+        return passwordLastSet;
+    }
+
+    public void setPasswordLastSet(long passwordLastSet) {
+        this.passwordLastSet = passwordLastSet;
+    }
+
+    public long getAccountExpires() {
+        return accountExpires;
+    }
+
+    public void setAccountExpires(long accountExpires) {
+        this.accountExpires = accountExpires;
+    }
+
+    public long getPasswordCanChange() {
+        return passwordCanChange;
+    }
+
+    public void setPasswordCanChange(long passwordCanChange) {
+        this.passwordCanChange = passwordCanChange;
+    }
+
+    public long getPasswordMustChange() {
+        return passwordMustChange;
+    }
+
+    public void setPasswordMustChange(long passwordMustChange) {
+        this.passwordMustChange = passwordMustChange;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getUserName() {
+        return userName;
+    }
+
+    public void setUserName(RPCUnicodeString.NonNullTerminated userName) {
+        this.userName = userName;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(RPCUnicodeString.NonNullTerminated fullName) {
+        this.fullName = fullName;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getHomeDirectory() {
+        return homeDirectory;
+    }
+
+    public void setHomeDirectory(RPCUnicodeString.NonNullTerminated homeDirectory) {
+        this.homeDirectory = homeDirectory;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getHomeDirectoryDrive() {
+        return homeDirectoryDrive;
+    }
+
+    public void setHomeDirectoryDrive(RPCUnicodeString.NonNullTerminated homeDirectoryDrive) {
+        this.homeDirectoryDrive = homeDirectoryDrive;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getScriptPath() {
+        return scriptPath;
+    }
+
+    public void setScriptPath(RPCUnicodeString.NonNullTerminated scriptPath) {
+        this.scriptPath = scriptPath;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getProfilePath() {
+        return profilePath;
+    }
+
+    public void setProfilePath(RPCUnicodeString.NonNullTerminated profilePath) {
+        this.profilePath = profilePath;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getAdminComment() {
+        return adminComment;
+    }
+
+    public void setAdminComment(RPCUnicodeString.NonNullTerminated adminComment) {
+        this.adminComment = adminComment;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getWorkStations() {
+        return workStations;
+    }
+
+    public void setWorkStations(RPCUnicodeString.NonNullTerminated workStations) {
+        this.workStations = workStations;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getUserComment() {
+        return userComment;
+    }
+
+    public void setUserComment(RPCUnicodeString.NonNullTerminated userComment) {
+        this.userComment = userComment;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(RPCUnicodeString.NonNullTerminated parameters) {
+        this.parameters = parameters;
+    }
+
+    public RPCShortBlob getLmOwfPassword() {
+        return lmOwfPassword;
+    }
+
+    public void setLmOwfPassword(RPCShortBlob lmOwfPassword) {
+        this.lmOwfPassword = lmOwfPassword;
+    }
+
+    public RPCShortBlob getNtOwfPassword() {
+        return ntOwfPassword;
+    }
+
+    public void setNtOwfPassword(RPCShortBlob ntOwfPassword) {
+        this.ntOwfPassword = ntOwfPassword;
+    }
+
+    public RPCUnicodeString.NonNullTerminated getPrivateData() {
+        return privateData;
+    }
+
+    public void setPrivateData(RPCUnicodeString.NonNullTerminated privateData) {
+        this.privateData = privateData;
+    }
+
+    public SAMPRSRSecurityDescriptor getSecurityDescriptor() {
+        return securityDescriptor;
+    }
+
+    public void setSecurityDescriptor(SAMPRSRSecurityDescriptor securityDescriptor) {
+        this.securityDescriptor = securityDescriptor;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getPrimaryGroupId() {
+        return primaryGroupId;
+    }
+
+    public void setPrimaryGroupId(int primaryGroupId) {
+        this.primaryGroupId = primaryGroupId;
+    }
+
+    public int getUserAccountControl() {
+        return userAccountControl;
+    }
+
+    public void setUserAccountControl(int userAccountControl) {
+        this.userAccountControl = userAccountControl;
+    }
+
+    public int getWhichFields() {
+        return whichFields;
+    }
+
+    public void setWhichFields(int whichFields) {
+        this.whichFields = whichFields;
+    }
+
+    public SAMPRLogonHours getLogonHours() {
+        return logonHours;
+    }
+
+    public void setLogonHours(SAMPRLogonHours logonHours) {
+        this.logonHours = logonHours;
+    }
+
+    public short getBadPasswordCount() {
+        return badPasswordCount;
+    }
+
+    public void setBadPasswordCount(short badPasswordCount) {
+        this.badPasswordCount = badPasswordCount;
+    }
+
+    public short getLogonCount() {
+        return logonCount;
+    }
+
+    public void setLogonCount(short logonCount) {
+        this.logonCount = logonCount;
+    }
+
+    public short getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(short countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public short getCodePage() {
+        return codePage;
+    }
+
+    public void setCodePage(short codePage) {
+        this.codePage = codePage;
+    }
+
+    public boolean isLmPasswordPresent() {
+        return lmPasswordPresent;
+    }
+
+    public void setLmPasswordPresent(boolean lmPasswordPresent) {
+        this.lmPasswordPresent = lmPasswordPresent;
+    }
+
+    public boolean isNtPasswordPresent() {
+        return ntPasswordPresent;
+    }
+
+    public void setNtPasswordPresent(boolean ntPasswordPresent) {
+        this.ntPasswordPresent = ntPasswordPresent;
+    }
+
+    public boolean isPasswordExpired() {
+        return passwordExpired;
+    }
+
+    public void setPasswordExpired(boolean passwordExpired) {
+        this.passwordExpired = passwordExpired;
+    }
+
+    public boolean isPrivateDataSensitive() {
+        return privateDataSensitive;
+    }
+
+    public void setPrivateDataSensitive(boolean privateDataSensitive) {
+        this.privateDataSensitive = privateDataSensitive;
+    }
 
     @Override
     public void unmarshalPreamble(PacketInput in) throws IOException {
