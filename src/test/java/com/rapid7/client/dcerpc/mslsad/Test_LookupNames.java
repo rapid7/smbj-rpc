14daa63e80a4ac668e4bfbea35d849eb7977f21d
Shreyas Khare <shreyas_khare@rapid7.com>
2017-11-21 18:21:35 -0500

    Fixing RPC leaks in lookupNames

--- src/test/java/com/rapid7/client/dcerpc/mslsad/Test_LookupNames.java
+++ src/test/java/com/rapid7/client/dcerpc/mslsad/Test_LookupNames.java
@@ -19,17 +19,24 @@
 
 package com.rapid7.client.dcerpc.mslsad;
 
+import com.rapid7.client.dcerpc.dto.SID;
 import com.rapid7.client.dcerpc.mslsad.messages.LsarLookupNamesRequest;
 import com.rapid7.client.dcerpc.mslsad.messages.LsarLookupNamesResponse;
 import com.rapid7.client.dcerpc.mslsad.objects.LSAPRReferencedDomainList;
 import com.rapid7.client.dcerpc.mslsad.objects.LSAPRTranslatedSIDs;
 import com.rapid7.client.dcerpc.objects.ContextHandle;
 import com.rapid7.client.dcerpc.objects.RPCSID;
+import com.rapid7.client.dcerpc.transport.RPCTransport;
 import java.io.IOException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class Test_LookupNames {
     @Rule
@@ -62,5 +69,36 @@ public class Test_LookupNames {
         final String[] names = {"Administrator"};
         final LsarLookupNamesRequest request = new LsarLookupNamesRequest(fakePolicyHandle, names);
         assertEquals(request.toHexString(), "000000008e3039708fdd9f488f9665426d0d9c5701000000010000001a001a00000002000d000000000000000d000000410064006d0069006e006900730074007200610074006f007200000000000000000000000100000000000000");
+    }
+
+    //This test is to verify that the Service correctly sets invalid SIDs to null from a valid response
+    @Test
+    public void testLookupNamesService()
+        throws IOException {
+        final RPCTransport transport = mock(RPCTransport.class);
+        final byte[] fakePolicyHandle = "000000008e3039708fdd".getBytes();
+        final LocalSecurityAuthorityService localSecurityAuthorityService = new LocalSecurityAuthorityService(transport);
+
+        final LsarLookupNamesResponse response = new LsarLookupNamesResponse();
+
+        response.fromHexString(
+                "00000200010000000400020020000000010000001a001c00080002000c0002000e000000000000000d0000005700310030002d0045004e0054002d005800360034002d005500000004000000010400000000000515000000a43cb4affe0503bd73de0f3504000000100002000400000001000000f40100000000000001000000e90300000000000001000000f5010000000000000800000000000000ffffffff0300000007010000");
+
+        when(transport.call(any(LsarLookupNamesRequest.class))).thenReturn(response);
+
+        SID[] SIDs = localSecurityAuthorityService.lookupNames(fakePolicyHandle, (String[]) null);
+
+        SID expectedDomainSID = new SID(
+                (byte) 1,
+                new byte[]{0,0,0,0,0,5},
+                new long[]{21, 2947824804L, 3171091966L, 890232435L});
+
+        SID[] expectedSIDs = new SID[4];
+        expectedSIDs[0] = expectedDomainSID.resolveRelativeID(500);
+        expectedSIDs[1] = expectedDomainSID.resolveRelativeID(1001);
+        expectedSIDs[2] = expectedDomainSID.resolveRelativeID(501);
+        expectedSIDs[3] = null;
+
+        assertArrayEquals(SIDs, expectedSIDs);
     }
 }
