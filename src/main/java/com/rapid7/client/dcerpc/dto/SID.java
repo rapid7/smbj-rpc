db0e296fac9fdd998147f12b691fd6ac53501741
Taylor Osmun <Taylor_Osmun@rapid7.com>
2017-11-22 14:10:54 -0500

    Comments

--- src/main/java/com/rapid7/client/dcerpc/dto/SID.java
+++ src/main/java/com/rapid7/client/dcerpc/dto/SID.java
@@ -90,7 +90,7 @@ public class SID {
     /**
      * @param relativeID The relative identifier (RID) used to construct a new {@link SID} object.
      * @return A new {@link SID} object as a clone of this object, with the provided relativeID appended to the subauthorities.
-     * Use of this method is typically only useful in situations where this SID represents an issue (i.e. domain).
+     * Use of this method is typically only useful in situations where this SID represents an issuer (i.e. domain).
      */
     public SID resolveRelativeID(final long relativeID) {
         final byte[] identifierAuthority = Arrays.copyOf(this.identifierAuthority, this.identifierAuthority.length);
