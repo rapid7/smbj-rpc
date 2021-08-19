8e450111119775ce28ce148797cbff3207009d51
Adam Smith <adam_smith@rapid7.com>
2021-08-19 13:12:11 +0100

    Add integration tests for SRVSVC

--- src/integration-test/java/IntegrationTestsIT.java
+++ src/integration-test/java/IntegrationTestsIT.java
@@ -1,4 +1,5 @@
 import com.rapid7.client.dcerpc.msrrp.RegistryService;
+import com.rapid7.client.dcerpc.mssrvs.ServerService;
 import com.rapid7.client.dcerpc.transport.RPCTransport;
 import com.rapid7.client.dcerpc.transport.SMBTransportFactories;
 import com.hierynomus.mssmb2.SMB2Dialect;
@@ -15,6 +16,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.images.builder.ImageFromDockerfile;
@@ -23,7 +25,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
-class WinregIT
+class IntegrationTestsIT
 {
    private static final Path DOCKER_BUILD_CONTEXT = Paths.get("src", "integration-test", "resources", "docker-image");
 
@@ -50,6 +52,26 @@ class WinregIT
 
          assertEquals(dialect, smbConnection.getNegotiatedProtocol().getDialect());
          assertEquals(shouldExist, registryService.doesKeyExist("HKLM", keyPath));
+      }
+   }
+
+   @ParameterizedTest
+   @EnumSource(value = SMB2Dialect.class, names = {"SMB_2_0_2", "SMB_2_1", "SMB_3_0", "SMB_3_0_2", "SMB_3_1_1"})
+   @DisplayName("Test service service enumerates shares for different SMB protocols")
+   void testSRVSVCReturnsSharesForEachSupportedSMBVersion(SMB2Dialect dialect)
+      throws IOException
+   {
+      final SmbConfig smbConfig = SmbConfig.builder().withSecurityProvider(new BCSecurityProvider()).withDialects(dialect).build();
+      final SMBClient smbClient = new SMBClient(smbConfig);
+      try (final Connection smbConnection = smbClient.connect("localhost", sambaContainer.getMappedPort(445))) {
+         final AuthenticationContext smbAuthenticationContext = new AuthenticationContext("smbj", "smbj".toCharArray(), "");
+         final Session session = smbConnection.authenticate(smbAuthenticationContext);
+
+         final RPCTransport transport = SMBTransportFactories.SRVSVC.getTransport(session);
+         final ServerService serverService = new ServerService(transport);
+
+         assertEquals(dialect, smbConnection.getNegotiatedProtocol().getDialect());
+         assertEquals(5, serverService.getShares0().size());
       }
    }
 
